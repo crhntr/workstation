3473aeabd083b9df948f7d88cabc9e4411c3da0f
Christopher Hunter <hunter.christopher@icloud.com>
2018-12-23 18:39:56 -0800

    Fix Order Brewfile Copy Before brew bundle

--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 function main() {
-  brew bundle --global
   cp gitconfig ~/.gitconfig
   cp profile.sh ~/.bash_profile
   cp vimrc ~/.vimrc
   cp Brewfile ~/.Brewfile
   cp greeting.sh ~/.greeting.sh
   cp gitignore_global ~/.gitignore_global
+  
+  brew bundle --global
 }
 
 main "$@"
