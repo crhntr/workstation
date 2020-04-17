335d0e6493f74dce65aee0d3c5f6ccd18bbae031
Christopher Hunter <hunter.christopher@icloud.com>
2020-04-17 12:29:58 -0700

    support unicode in tmux

--- profile.sh
+++ profile.sh
@@ -28,6 +28,7 @@ alias gp="git push"
 alias gup="git fetch --all && git pull --rebase --autostash"
 alias k="kubectl"
 alias ll="ls -al"
+alias tmux='tmux -u'
 
 function download() {
   aria2c -x 16 -s 16 "$@"
