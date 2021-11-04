4b8378391d2613934de068fd9095cee8f35210fc
Christopher Hunter <hunter.christopher@icloud.com>
2021-11-03 22:54:56 -0700

    update profile

--- profile.sh
+++ profile.sh
@@ -14,7 +14,6 @@ export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; histor
 
 export GIT_EDITOR="nvim"
 export EDITOR="nvim"
-export GOPATH=$HOME
 
 export PATH="${PATH}:${GOPATH}/bin"
 export PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH"
@@ -32,14 +31,8 @@ function download() {
   aria2c -x 16 -s 16 "$@"
 }
 
-function www_github() {
-  open https://github.com/$1
-}
-
 function git_branch() {
   git branch 2>/dev/null | grep '^*' | colrm 1 2
 }
-
-export PS1="\$ "
 
 $HOME/.greeting.sh
